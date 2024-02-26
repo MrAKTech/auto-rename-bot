aa524e27ac9bf873c280686395310c397117f451
Manjunath Bhat <86521081+Bhatmanjusms@users.noreply.github.com>
2024-02-26 14:38:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ API_HASH = environ.get("API_HASH", "0c20c4265501492a1513f91755acd42b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6131001082:AAFZ5UMl2NOSw2lOokyBfmXLfkQa0H-z778")
 ADMIN = int(environ.get("ADMIN", "399726799"))          
 CAPTION = environ.get("CAPTION", "")
-DESTINATION_CHANNEL = int(environ.get("DESTINATION_CHANNEL", "-1001919589838"))
-SOURCE_CHANNEL = int(environ.get("SOURCE_CHANNEL", "-1001960862288"))
+DESTINATION_CHANNEL = int(environ.get("DESTINATION_CHANNEL", "-1002112267615"))
+SOURCE_CHANNEL = int(environ.get("SOURCE_CHANNEL", "-1002007777333"))
 DB_URL  = environ.get("DB_URL","mongodb+srv://abcd:abcd@cluster0.jmkqvnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # ⚠️ Required
 
 # for thumbnail ( back end is MrMKN brain 😉)

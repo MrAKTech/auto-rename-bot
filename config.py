8414242cd0c56b759734065fd4b5916a83eb6063
Manjunath Bhat <86521081+Bhatmanjusms@users.noreply.github.com>
2024-02-26 14:31:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", "12124605"))
-API_HASH = environ.get("API_HASH", "5cf3577d85fd02286535ec2296934287")
+API_ID = int(environ.get("API_ID", "977080"))
+API_HASH = environ.get("API_HASH", "0c20c4265501492a1513f91755acd42b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6113221657:AAGezKFTnAYwOyUNMX1FwLtPMVcybouxOH8")
-ADMIN = int(environ.get("ADMIN", "5927274758"))          
+ADMIN = int(environ.get("ADMIN", "399726799"))          
 CAPTION = environ.get("CAPTION", "")
 DESTINATION_CHANNEL = int(environ.get("DESTINATION_CHANNEL", "-1001919589838"))
 SOURCE_CHANNEL = int(environ.get("SOURCE_CHANNEL", "-1001960862288"))
-DB_URL  = environ.get("DB_URL","mongodb+srv://Approve_two:Approve_two@cluster0.vnkahxn.mongodb.net/?retryWrites=true&w=majority") # ⚠️ Required
+DB_URL  = environ.get("DB_URL","mongodb+srv://abcd:abcd@cluster0.jmkqvnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # ⚠️ Required
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
 
-ACCESS = ["5927274758", "1584694165"]
+ACCESS = [""]
